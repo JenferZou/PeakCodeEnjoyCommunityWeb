d849bd01ac321de41bdf1b6fbdb93e00598c33c3
Jenfer <1634114623@qq.com>
2023-11-26 16:36:39 +0800

    zblog前端1.0

---
+++ src/components/PageDataList.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div v-if="!loading&&dataSource.list!=null&&dataSource.list.length==0">
+        <Nodata></Nodata>
+    </div>
+    <div class="skeleton" v-if="loading">
+        <el-skeleton :row="3" animated></el-skeleton>
+    </div>
+
+  <div v-for="item in dataSource.list" v-if="!loading">
+      <slot :data="item"></slot>
+  </div>
+  <div class="pagination">
+      <el-pagination
+              :page-size="dataSource.pageSize"
+              background
+              :current-page.syn="dataSource.pageNo"
+              :pager-count="11"
+              layout="prev, pager, next"
+              :total="dataSource.totalCount"
+              @current-change="handlePageNoChange"
+              style="text-align: right"
+      />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    dataSource:{
+        type:Object,
+    },
+    loading:{
+        type:Boolean,
+    }
+});
+
+const emit = defineEmits(["loadData"])
+const handlePageNoChange = (pageNo)=>{
+    props.dataSource.pageNo = pageNo;
+    emit("loadData");
+    
+};
+
+</script>
+
+<style lang="scss">
+.skeleton{
+  padding: 15px;
+}
+</style>
